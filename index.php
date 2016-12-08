5dee4154c45fb83c3d34f8c15efc2bb6e1e433d3
Graham Thomas <Graham_Thomas@intuit.com>
2016-12-08 00:02:46 -0800

    Implemented color change for incorrect input

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
 
 <head>
-  <title>PHP Test</title>
+  <title>Tip Calculator</title>
   <link rel="stylesheet" type="text/css" href="style.css">
   <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-  </head>
+</head>
 
 <body>
   <div class="calculator_style">
@@ -12,39 +12,48 @@
     <form id="tip_calculator_form" action="index.php" method="post">
       <?php
 # Allow values to persist between submissions if all values are not filled
-    if (!isset($_POST['bill_total']))
+    if (isset($_POST['bill_total']) && intval($_POST['bill_total']) > 0)
     {
-        $previous_bill_total = 0;
-    }
-    else{
+        $error_in_bill_total = false;
         $previous_bill_total = $_POST['bill_total'];
     }
-
+    else{
+        $error_in_bill_total = true;
+        $previous_bill_total = 0;
+    }
+    
     if (!isset($_POST['tip_percentage']))
     {
+        $error_in_tip_radio = true;
         $previous_tip_percentage = 0;
     }
     else
     {
+        $error_in_tip_radio = false;
         $previous_tip_percentage = $_POST['tip_percentage'];
     }
-
-    if(isset($_POST['tip_percentage']) && isset($_POST['bill_total']) && $_POST['bill_total'] != '0')
+    
+    if(isset($_POST['tip_percentage']) && isset($_POST['bill_total']) && intval($_POST['bill_total']) > 0)
     {
         $previous_bill_total = 0;
         $previous_tip_percentage = 0;
     }
     
     ?>
-        Bill Subtotal:
-        <input type="text" name="bill_total" value=<?php echo $previous_bill_total?>>
-        </br>
-        </br>
-        Tip Percentage:
-        </br>
-        </br>
-        <!--- Change these to php -->
+
+
         <?php
+    #Sets color if the previous input was incorrect
+        if ($error_in_bill_total) $billing_text_type = "error_text";
+    if (!$error_in_bill_total) $billing_text_type = "normal_text";
+    echo "<p class=\"$billing_text_type\"> Bill Subtotal: <input type=\"text\"  value=\"$previous_bill_total\"></p>";
+    
+    if ($error_in_tip_radio) $billing_text_type = "error_text";
+    if (!$error_in_tip_radio) $billing_text_type = "normal_text";
+    echo "<p class=\"$billing_text_type\"> Tip Percentage: </p>" ?>
+
+          <?php
+          #Create radio buttons use PHP For Loop
     for($x = 0; $x < 3; $x++)
     {
         $is_checked = "";
@@ -54,13 +63,13 @@
         echo "<input type=\"radio\" name=\"tip_percentage\" value=$tip_percent_to_display . \"%\" $is_checked> ";
     }
     ?>
-          </br>
-          </br>
-          <input type="submit" name="submit" value="">
-          </br>
+            </br>
+            </br>
+            <input type="submit" name="submit" value="">
+            </br>
     </form>
 
-      <?php
+    <?php
     if(isset($_POST["submit"]))
     {
         if(validate_bill_total() && validate_tip_percentage())
